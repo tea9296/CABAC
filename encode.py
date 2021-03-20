4daafc2ffcc51752b415330517779ee422e6963c
tea9296 <tea9296@gmail.com>
2021-03-20 08:43:02 +0800

    implement arithmetic coding in encode.py

--- encode.py
+++ encode.py
@@ -1,13 +1,5 @@
 import numpy as np
-from decimal import Decimal
-def dec2bin(x):
-    x -= int(x)
-    bins = []
-    while x:
-        x *= 2
-        bins.append(1 if x>=1. else 0)
-        x -= int(x)
-    return bins
+
 
 def bins2dec(bins):
     count=1
@@ -16,6 +8,23 @@ def bins2dec(bins):
         x = x + bin*0.5/count
         count*=2
     return x
+
+def readProb10bits(bins):
+    x = 0
+    count = 1
+    numb = 10
+    #if len(bins) <numb:
+    #    bitsNum = len(bins)
+    #else:
+    #    bitsNum = numb
+    while len(bins) < numb:
+        bins.append(0)
+
+    for i in range(numb):
+
+        x = x + bins[numb-i-1]*count
+        count *= 2
+    return x, numb
 
 def ProbabilityModel():
     pModel =[]
@@ -27,162 +36,182 @@ def ProbabilityModel():
         else:
             prob = prob*alpha
             pModel.append(prob)
-    print(pModel)
     return pModel
 
 
+def Encode(binStrs):
+    #binStr = "10101111"
+    pModel = ProbabilityModel()
+    encodeProbCodes = []
+    ### for exch block###
+    for binStr in binStrs:
+        MPS = '0'
+        idxP = 0
+        intervalStart = 0
+        intervalRange = 1024
+        res = []
+        bitOutstand = 0
+        for binValue in binStr:
+            intervalRangeLPS = pModel[idxP] * intervalRange
+            intervalRange = intervalRange - intervalRangeLPS
+            if binValue == int(MPS):
+                # intervalRange = intervalRange - (intervalRange*pModel[idxP])  ###R(i+1) = R(i) - R(LPS)
+                # intervalStart = intervalStart
+                if idxP <= 62:
+                    idxP += 1
 
-binStr = "00001001000000100010101010101111"
-print(len(binStr))
-pModel = ProbabilityModel()
-MPS = '0'
-idxP = 0
-### for exch block###
-intervalStart = 0
-intervalRange = 1024
-res = []
-bitOutstand=0
-for binValue in binStr:
-    if binValue == MPS:
-        intervalRange = intervalRange - (intervalRange*pModel[idxP])  ###R(i+1) = R(i) - R(LPS)
-        intervalStart = intervalStart
-        if idxP <= 62:
-            idxP += 1
-
-    else:
-        intervalStart = intervalStart + (intervalRange - (intervalRange*pModel[idxP]))
-        intervalRange = intervalRange*pModel[idxP]
-        if idxP > 0:
-            idxP -= 1
-        elif idxP == 0:
-            if MPS == '0':
-                MPS = '1'
             else:
-                MPS = '0'
+                intervalStart = intervalStart + intervalRange
+                intervalRange = intervalRangeLPS
+                if idxP > 0:
+                    idxP -= 1
+                elif idxP == 0:
+                    if MPS == '0':
+                        MPS = '1'
+                    else:
+                        MPS = '0'
 
-    ### renormalization not finished ###
-    while intervalRange < 256:
-        if intervalStart < 256:
-            res.append(0)
-            if bitOutstand>0:
+            ### renormalization ###
+            while intervalRange < 256:
+                if intervalStart < 256:
+                    res.append(0)
+                    while bitOutstand > 0:
+                        res.append(1)
+                        bitOutstand -= 1
+                    intervalStart = int(intervalStart * 2)
+                    intervalRange = int(intervalRange * 2)
+                elif intervalStart >= 512:
+                    intervalStart = int(intervalStart - 512)
+                    res.append(1)
+                    while bitOutstand > 0:
+                        res.append(0)
+                        bitOutstand -= 1
+                    intervalStart = int(intervalStart * 2)
+                    intervalRange = int(intervalRange * 2)
+                elif intervalStart < 512:
+                    intervalStart = int(intervalStart - 256)
+                    intervalStart = int(intervalStart * 2)
+                    intervalRange = int(intervalRange * 2)
+                    bitOutstand += 1
+
+        ### end of encoding ###
+        intervalRange = intervalRange - 2
+        intervalStart = intervalStart + intervalRange
+        intervalRange = 2
+
+        while intervalRange < 256:
+            if intervalStart < 256:
+                res.append(0)
+                while bitOutstand > 0:
+                    res.append(1)
+                    bitOutstand -= 1
+                intervalStart = int(intervalStart * 2)
+                intervalRange = int(intervalRange * 2)
+            elif intervalStart >= 512:
+                intervalStart = int(intervalStart - 512)
                 res.append(1)
-                bitOutstand -= 1
-            intervalStart = int(intervalStart*2)
-            intervalRange = int(intervalRange*2)
-        elif intervalStart >= 512:
-            intervalStart = int(intervalStart - 512)
+                while bitOutstand > 0:
+                    res.append(0)
+                    bitOutstand -= 1
+                intervalStart = int(intervalStart * 2)
+                intervalRange = int(intervalRange * 2)
+            elif intervalStart < 512:
+                intervalStart = int(intervalStart - 256)
+                intervalStart = int(intervalStart * 2)
+                intervalRange = int(intervalRange * 2)
+                bitOutstand += 1
+
+        if intervalStart / 512 >= 1:
             res.append(1)
-            if bitOutstand > 0:
+            while bitOutstand > 0:
                 res.append(0)
                 bitOutstand -= 1
-            intervalStart = int(intervalStart * 2)
-            intervalRange = int(intervalRange * 2)
-        elif intervalStart < 512:
-            intervalStart = int(intervalStart - 256)
-            intervalStart = int(intervalStart * 2)
-            intervalRange = int(intervalRange * 2)
-            bitOutstand += 1
-
-
-### end of encoding ###
-intervalStart = intervalStart + intervalRange
-intervalRange = 2
-
-while intervalRange < 256:
-    if intervalStart < 256:
-        res.append(0)
-        if bitOutstand > 0:
-            res.append(1)
-            bitOutstand -= 1
-        intervalStart = int(intervalStart * 2)
-        intervalRange = int(intervalRange * 2)
-    elif intervalStart >= 512:
-        intervalStart = int(intervalStart - 512)
-        res.append(1)
-        if bitOutstand > 0:
+        else:
             res.append(0)
-            bitOutstand -= 1
-        intervalStart = int(intervalStart * 2)
-        intervalRange = int(intervalRange * 2)
-    elif intervalStart < 512:
-        intervalStart = int(intervalStart - 256)
-        intervalStart = int(intervalStart * 2)
-        intervalRange = int(intervalRange * 2)
-        bitOutstand += 1
-
-if intervalStart/512 >= 1:
-    res.append(1)
-else:
-    res.append(0)
-
-print(intervalRange)
-print(intervalStart)
-print(res)
-print(len(res))
-print(bins2dec(res))
-"""
-### find shortest value in interval ###
-print(dec2bin(intervalStart+intervalRange))
-print(dec2bin(intervalStart))
-endLi = dec2bin(intervalStart+intervalRange)
-startLi = dec2bin(intervalStart)
-res=[]
-for i in range(len(endLi)):
-    if endLi[i] != startLi[i]:
-        res.append(startLi[i])
-        while startLi[i+1] != 0:
+            while bitOutstand > 0:
+                res.append(1)
+                bitOutstand -= 1
+        if intervalRange / 128 >= 2:
             res.append(1)
-            i = i+1
-        res.append(1)
-        break
-    else:
-        res.append(startLi[i])
-
-print(res)
-print(bins2dec(res))
-"""
+        else:
+            res.append(0)
 
 
+        encodeProbCodes.append(res)
+
+    return encodeProbCodes
 
 
-
-
-"""
 
 ####decoding########
+def Decode(encodeProbCodes, encodeBitsNum):
 
-#prob = bins2dec(res)
-prob = (intervalRange/2+intervalStart)
-MPS = '0'
-idxP = 0
-intervalStart = 0
-intervalRange = 1024
-ans = []
-for count in range(16):
-    if prob < intervalStart + pModel[idxP]*intervalRange:
-        ans.append(int(MPS))
-        intervalRange = intervalRange - (intervalRange*pModel[idxP])  ###R(i+1) = R(i) - R(LPS)
-        intervalStart = intervalStart
+    decodeCode = []
+    pModel = ProbabilityModel()
+    for encodeProbCode in encodeProbCodes:
+        prob, bits = readProb10bits(encodeProbCode)
+        intervalRange = 1024
+        intervalRangeLPS = 0
+        MPS = '0'
+        idxP = 0
+        ans = []
+        for count in range(encodeBitsNum):
+            intervalRangeLPS = int(pModel[idxP] * intervalRange)
+            intervalRange = intervalRange - intervalRangeLPS
+            if prob >= intervalRange:
+                if MPS == '0':
+                    ans.append(1)
+                else:
+                    ans.append(0)
+                prob = prob - intervalRange
+                intervalRange = intervalRangeLPS
+                # intervalStart = intervalStart + (intervalRange - (intervalRange*pModel[idxP]))
+                # intervalRange = intervalRange*pModel[idxP]
 
-        if idxP <= 62:
-            idxP += 1
+                if idxP > 0:
+                    idxP -= 1
+                elif idxP == 0:
+                    if MPS == '0':
+                        MPS = '1'
+                    else:
+                        MPS = '0'
 
-    else:
-        if MPS == '0':
+
+            else:
+                ans.append(int(MPS))
+                # intervalRange = intervalRange - (intervalRange*pModel[idxP])  ###R(i+1) = R(i) - R(LPS)
+                # intervalStart = intervalStart
+
+                if idxP <= 62:
+                    idxP += 1
+
+            ###renormalization###
+
+            while intervalRange < 256:
+                intervalRange = intervalRange * 2
+                bits += 1
+                prob = (prob * 2)
+                if bits <= len(encodeProbCode):
+                    prob = prob + encodeProbCode[bits - 1]
+                # print(prob)
+
+        """
+        ### decode terminate
+        intervalRange = intervalRange - 2
+        if prob >= intervalRange:
             ans.append(1)
         else:
             ans.append(0)
-        intervalStart = intervalStart + (intervalRange - (intervalRange*pModel[idxP]))
-        intervalRange = intervalRange*pModel[idxP]
+            while intervalRange < 256:
+                intervalRange = intervalRange*2
+                #print(prob)
+                bits += 1
+               #print("res"+str(res))
+                prob = (prob * 2)
+                if bits <= len(res):
+                    prob = prob + res[bits-1]
 
-        if idxP > 0:
-            idxP -= 1
-        elif idxP == 0:
-            if MPS == '0':
-                MPS = '1'
-            else:
-                MPS = '0'
+        """
 
-
-#print(ans)
-"""
+        decodeCode.append(ans)
+    return decodeCode
--- main.py
+++ main.py
@@ -1,12 +1,67 @@
 import numpy as np
+import cv2
+import encode
+import time
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def FixedLengthBinarization(element, length):
+    result = []
+    value = 2**(length-1)
+    for i in range(length):
+        if element/value >=1:
+            element = element - value
+            result.append(1)
+        else:
+            result.append(0)
+        value /= 2
+    return result
+
+def GenerateBineryToDecimal(decodeCode):
+    result = []
+
+    for binStr in decodeCode:
+        pixelValue = 0
+        value = 2 ** (len(decodeCode[0]) - 1)
+        for bin in binStr:
+            pixelValue = pixelValue + bin*value
+            value /= 2
+        result.append(pixelValue)
+    return result
+
+
+img = cv2.imread("pic1.jpg")
+blockSize = 8
+
+
+
+### Change picture into grayscale and 1D array
+grayPic = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+syntaxElements = np.array(grayPic.flatten())
+
+n, m = grayPic.shape[0], grayPic.shape[1]
+
+print("Picture length and width: "+str(grayPic.shape))
+print("Picture size: "+str(np.size(grayPic))+" bytes")
+### Binarization
+binStrs = []
+for i in syntaxElements:
+    binStrs.append(FixedLengthBinarization(i, 8))
+
+
+### encode and decode ###
+start = time.time()
+encodeProbCodes = encode.Encode(binStrs)
+decodeCode = encode.Decode(encodeProbCodes, blockSize)
+
+end = time.time()
+print("execute time: "+str(end - start))
+count=0
+for i in range(len(encodeProbCodes)):
+    count += len(encodeProbCodes[i])
+print("Compressed size: "+str(count/8) + " bytes")
+### regenerate picture ###
+
+result = np.array(GenerateBineryToDecimal(decodeCode)).reshape(n, m)
+cv2.imwrite('result.jpg', result)
